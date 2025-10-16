fabde08573e3027a3c46ecde9b2a93c79a81ceda
Prithxvhie44 <44prithviraj44@gmail.com>
2025-10-16 20:43:12 +0530

    Working chatbot through terminal

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,15 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+model=ChatGoogleGenerativeAI(api_key=api_key, model="gemini-2.5-flash")
+
+while True:
+    user_input = input("Enter your message (or 'exit' to quit): ")
+    if user_input.lower() == 'exit':
+        break
+    result=model.invoke(user_input)
+    print("Response:", result.content)
