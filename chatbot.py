9acc3e63e1b05be4a710e5607534a1779b320e5a
Prithxvhie44 <44prithviraj44@gmail.com>
2025-10-19 15:41:05 +0530

    history added

--- chatbot.py
+++ chatbot.py
@@ -4,12 +4,18 @@ import os
 
 load_dotenv()
 
+chat_history=[]
+
 api_key = os.getenv("GEMINI_API_KEY")
 model=ChatGoogleGenerativeAI(api_key=api_key, model="gemini-2.5-flash")
 
 while True:
     user_input = input("Enter your message (or 'exit' to quit): ")
+    chat_history.append(user_input)
     if user_input.lower() == 'exit':
         break
-    result=model.invoke(user_input)
+    result=model.invoke(chat_history)
+    chat_history.append(result.content)
     print("Response:", result.content)
+
+print(chat_history)
